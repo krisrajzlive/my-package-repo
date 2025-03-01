0370e11a2586a1d5f948330d69841d96cdd6c512
Rajarajacholan Krishnamurthy <krisrajz@live.com>
2025-03-01 13:22:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(){
+        this.message = "Hello World"
+        console.log(this.message)
+    }
+
+    getUserName(){}
+
+    getUserStatus(){}
+}
+
+module.exports = User;
